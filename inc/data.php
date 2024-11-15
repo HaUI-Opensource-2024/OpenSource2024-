b15ed295c103587d93e4c046820cc9daf38d9f83
RoanVanQuyen <roan.dev.backend@gmail.com>
2024-11-15 15:41:04 +0700

    chore(setting-page): Change Prefix in file "Data"

--- inc/data.php
+++ inc/data.php
@@ -1,53 +1,10 @@
 <?php
-function language_array() {
-    return [
-        'English'    => 'en',
-        'Afrikaans'  => 'af',
-        'Arabic'     => 'ar',
-        'Armenian'   => 'an',
-        'Bosnian'    => 'bs',
-        'Bulgarian'  => 'bg',
-        'Chinese'    => 'zh',
-        'Croatian'   => 'hr',
-        'Czech'      => 'cs',
-        'Danish'     => 'da',
-        'Dutch'      => 'nl',
-        'Estonian'   => 'et',
-        'Filipino'   => 'fil',
-        'Finnish'    => 'fi',
-        'French'     => 'fr',
-        'German'     => 'de',
-        'Greek'      => 'el',
-        'Hebrew'     => 'he',
-        'Hindi'      => 'hi',
-        'Hungarian'  => 'hu',
-        'Indonesian' => 'id',
-        'Italian'    => 'it',
-        'Japanese'   => 'ja',
-        'Korean'     => 'ko',
-        'Latvian'    => 'lv',
-        'Lithuanian' => 'lt',
-        'Malay'      => 'ms',
-        'Norwegian'  => 'no',
-        'Persian'    => 'fa',
-        'Polish'     => 'pl',
-        'Portuguese' => 'pt',
-        'Romanian'   => 'ro',
-        'Russian'    => 'ru',
-        'Serbian'    => 'sr',
-        'Slovak'     => 'sk',
-        'Slovenian'  => 'sl',
-        'Spanish'    => 'es',
-        'Swedish'    => 'sv',
-        'Thai'       => 'th',
-        'Turkish'    => 'tr',
-        'Ukrainian'  => 'uk',
-        'Urdu'       => 'ur',
-        'Vietnamese' => 'vi',
-    ];
+
+if ( !defined( 'ABSPATH' ) ) {
+    exit( 'You are not allowed' );
 }
 
-function haui_templates() {
+function hagen_templates() {
     $all_prompts = [
         [
             'title'       => 'Headline Generation',
@@ -168,6 +125,58 @@ function haui_templates() {
 
     ];
 
-    $new_prompts = apply_filters( PREFIX.'all_prompts', $all_prompts );
+    $new_prompts = apply_filters( 'hagen_all_prompts', $all_prompts );
+
+
     return array_merge( $all_prompts, $new_prompts );
+}
+
+// language set
+function hagen_language_array() {
+    return [
+        'English'    => 'en',
+        'Afrikaans'  => 'af',
+        'Arabic'     => 'ar',
+        'Armenian'   => 'an',
+        'Bosnian'    => 'bs',
+        'Bulgarian'  => 'bg',
+        'Chinese'    => 'zh',
+        'Croatian'   => 'hr',
+        'Czech'      => 'cs',
+        'Danish'     => 'da',
+        'Dutch'      => 'nl',
+        'Estonian'   => 'et',
+        'Filipino'   => 'fil',
+        'Finnish'    => 'fi',
+        'French'     => 'fr',
+        'German'     => 'de',
+        'Greek'      => 'el',
+        'Hebrew'     => 'he',
+        'Hindi'      => 'hi',
+        'Hungarian'  => 'hu',
+        'Indonesian' => 'id',
+        'Italian'    => 'it',
+        'Japanese'   => 'ja',
+        'Korean'     => 'ko',
+        'Latvian'    => 'lv',
+        'Lithuanian' => 'lt',
+        'Malay'      => 'ms',
+        'Norwegian'  => 'no',
+        'Persian'    => 'fa',
+        'Polish'     => 'pl',
+        'Portuguese' => 'pt',
+        'Romanian'   => 'ro',
+        'Russian'    => 'ru',
+        'Serbian'    => 'sr',
+        'Slovak'     => 'sk',
+        'Slovenian'  => 'sl',
+        'Spanish'    => 'es',
+        'Swedish'    => 'sv',
+        'Thai'       => 'th',
+        'Turkish'    => 'tr',
+        'Ukrainian'  => 'uk',
+        'Urdu'       => 'ur',
+        'Vietnamese' => 'vi',
+    ];
+
 }
